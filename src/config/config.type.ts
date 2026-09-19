f83624b8580275be1374be747c4b0167cc9cccf5
NitishPalyal <nitishpalyal248@gmail.com>
2026-09-19 21:45:03 +0530

    env config file and database instance file completed

---
+++ src/config/config.type.ts
@@ -0,0 +1,8 @@
+export type CONFIG = {
+  readonly FRONTEND_URL: string;
+  readonly DATABASE_URL: string;
+  readonly B2_BUCKET_NAME: string;
+  readonly B2_KEY_ID: string;
+  readonly B2_APPLICATION_KEY: string;
+  readonly B2_ENDPOINT: string;
+};
