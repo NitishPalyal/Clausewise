7eaf0a759a3e79a0b4da73d1ce59ea0f88082176
NitishPalyal <nitishpalyal248@gmail.com>
2026-09-20 11:01:26 +0530

    removed unused pakages

--- src/config/config.keys.ts
+++ src/config/config.keys.ts
@@ -5,6 +5,8 @@ dotenv.config();
 const configKeys: CONFIG = {
   FRONTEND_URL: process.env.FRONTEND_URL || "",
   DATABASE_URL: process.env.DATABASE_URL || "",
+  REDIS_HOST: process.env.REDIS_HOST || "",
+  REDIS_PORT: process.env.REDIS_PORT || "",
   B2_BUCKET_NAME: process.env.B2_BUCKET_NAME || "",
   B2_KEY_ID: process.env.B2_KEY_ID || "",
   B2_APPLICATION_KEY: process.env.B2_APPLICATION_KEY || "",
